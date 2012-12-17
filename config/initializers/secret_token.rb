0ff756ce692934ce31aaacba321f1451c5d13e88
ostap <otan256@gmail.com>
2012-12-17 13:19:56 +0200

    project base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoldDust::Application.config.secret_token = '50dfb5c26557808786617b10cbfa84a4e6ebce91425ac238660f8179cd93dd3f0a0118990b4cd1d36ce3025025d479cbeff9aecf4be00869a7c3babe4be57a01'
